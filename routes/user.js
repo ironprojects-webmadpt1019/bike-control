29118ba4369855bab27d48e5036e00b4ca9d482c
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-19 11:31:25 +0100

    Protect the /user route

--- routes/user.js
+++ routes/user.js
@@ -5,31 +5,23 @@ const User = require("../models/User");
 // Add LoggedIn Middleware
 const ensureLogin = require("connect-ensure-login");
 
-router.get(
-  "/settings",
-  ensureLogin.ensureLoggedIn("/auth/login"),
-  (req, res, next) => {
-    res.render("user/settings");
-  }
-);
+router.get("/settings", (req, res, next) => {
+  res.render("user/settings");
+});
 
-router.post(
-  "/settings",
-  ensureLogin.ensureLoggedIn("/auth/login"),
-  async (req, res, next) => {
-    const { username, firstName, lastName } = req.body;
-    const loggedUser = req.user;
+router.post("/settings", async (req, res, next) => {
+  const { username, firstName, lastName } = req.body;
+  const loggedUser = req.user;
 
-    // Update user in database
-    loggedUser.username = username;
-    loggedUser.name.first = firstName;
-    loggedUser.name.last = lastName;
+  // Update user in database
+  loggedUser.username = username;
+  loggedUser.name.first = firstName;
+  loggedUser.name.last = lastName;
 
-    await loggedUser.save();
+  await loggedUser.save();
 
-    req.flash("error", "Updated user!");
-    res.redirect("/user/settings");
-  }
-);
+  req.flash("error", "Updated user!");
+  res.redirect("/user/settings");
+});
 
 module.exports = router;
