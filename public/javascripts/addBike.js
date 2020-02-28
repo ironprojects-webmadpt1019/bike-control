eff9ba2c4539b8048ab2c61b8a6bab582c711796
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-28 14:10:04 +0100

    Add spinner meanwhile your are being located before adding a bike and disable the add location button to prevent errors

--- public/javascripts/addBike.js
+++ public/javascripts/addBike.js
@@ -2,32 +2,36 @@ const buttons = document.querySelectorAll(".park-bike");
 const modal = document.querySelector(".modal");
 const yes = document.querySelector(".modal-yes");
 const no = document.querySelector(".modal-no");
+const spin = document.querySelector(".spinner");
 
 buttons.forEach(e => {
   e.addEventListener("click", event => {
+    e.disabled = true;
+    spin.style.visibility = "initial";
     const id = event.currentTarget.children[0].innerHTML;
-    console.log("Enter location");
+    //console.log("Enter location");
     geolocation().then(geo => {
-      console.log("geolocated");
+      //console.log("geolocated");
       const { latitude, longitude } = geo.coords;
       const params = { latitude, longitude, id };
       axios.post("/user/bikes/location/check", params).then(res => {
-        console.log(res.data);
+        //console.log(res.data);
         const incidents = res.data.reports;
         if (incidents.length > 5) {
-          console.log("too much incidents");
-          console.log(modal);
+          //console.log("too much incidents");
+          //console.log(modal);
+          spin.style.visibility = "hidden";
           modal.style.visibility = "initial";
           modal.style.opacity = "1";
           yes.addEventListener("click", event => {
             axios.post("/user/bikes/location", params).then(res => {
-              console.log("bike-park");
+              //console.log("bike-park");
               window.location.href = "/map";
             });
           });
         } else {
           axios.post("/user/bikes/location", params).then(res => {
-            console.log("bike-park");
+            //console.log("bike-park");
             window.location.href = "/map";
           });
         }
@@ -53,4 +57,7 @@ yes.addEventListener("click", event => {
 no.addEventListener("click", event => {
   modal.style.visibility = "hidden";
   modal.style.opacity = "0";
+  buttons.forEach(e => {
+    e.disabled = false;
+  });
 });
