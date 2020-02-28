a52c7e69c34c6dce1ba023b0a31f9c15fc323ca7
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-28 19:08:44 +0100

    Add modal to confirm before parking and set the spinner into a partial

--- public/javascripts/addBike.js
+++ public/javascripts/addBike.js
@@ -1,38 +1,49 @@
 const buttons = document.querySelectorAll(".park-bike");
 const modal = document.querySelector(".modal");
+const title = modal.children[0];
+const msg = modal.children[1];
 const yes = document.querySelector(".modal-yes");
 const no = document.querySelector(".modal-no");
 const spin = document.querySelector(".spinner");
+const wrapper = document.querySelector(".wrapper");
 
 buttons.forEach(e => {
   e.addEventListener("click", event => {
     e.disabled = true;
+    wrapper.style.visibility = "initial";
+    wrapper.style.opacity = "1";
     spin.style.visibility = "initial";
+    spin.style.opacity = "1";
     const id = event.currentTarget.children[0].innerHTML;
-    //console.log("Enter location");
     geolocation().then(geo => {
-      //console.log("geolocated");
       const { latitude, longitude } = geo.coords;
       const params = { latitude, longitude, id };
       axios.post("/user/bikes/location/check", params).then(res => {
-        //console.log(res.data);
         const incidents = res.data.reports;
-        if (incidents.length > 5) {
-          //console.log("too much incidents");
-          //console.log(modal);
+        if (incidents.length >= 5) {
           spin.style.visibility = "hidden";
+          title.innerHTML = `There are ${incidents.length} reported issues within 2km`;
+          msg.innerHTML = "Are you sure you want to park here?";
           modal.style.visibility = "initial";
           modal.style.opacity = "1";
           yes.addEventListener("click", event => {
             axios.post("/user/bikes/location", params).then(res => {
-              //console.log("bike-park");
               window.location.href = "/map";
             });
           });
         } else {
-          axios.post("/user/bikes/location", params).then(res => {
-            //console.log("bike-park");
-            window.location.href = "/map";
+          spin.style.visibility = "hidden";
+          title.innerHTML = `There are only ${incidents.length} reported issues within 2km`;
+          msg.innerHTML =
+            "You're probably safe, if anything happens do not hessitate to report it";
+          yes.innerHTML = "Ok!";
+          no.innerHTML = "I'll wait";
+          modal.style.visibility = "initial";
+          modal.style.opacity = "1";
+          yes.addEventListener("click", event => {
+            axios.post("/user/bikes/location", params).then(res => {
+              window.location.href = "/map";
+            });
           });
         }
       });
@@ -40,23 +51,11 @@ buttons.forEach(e => {
   });
 });
 
-yes.addEventListener("click", event => {
-  const id = event.currentTarget.children[0].innerHTML;
-  console.log("Enter");
-  geolocation().then(geo => {
-    console.log("geolocated");
-    const { latitude, longitude } = geo.coords;
-    const params = { latitude, longitude, id };
-    axios.post("/user/bikes/location", params).then(res => {
-      console.log("bike-park");
-      window.location.href = "/map";
-    });
-  });
-});
-
 no.addEventListener("click", event => {
   modal.style.visibility = "hidden";
   modal.style.opacity = "0";
+  wrapper.style.visibility = "hidden";
+  wrapper.style.opacity = "0";
   buttons.forEach(e => {
     e.disabled = false;
   });
